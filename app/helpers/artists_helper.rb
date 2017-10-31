a5103038ebe1c3d2bc05b6ff5579a638b82fa59a
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-10-30 18:01:43 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,16 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, artist)
+    if song.artist.nil?
+      #in ruby this will create a select box tag
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      #in ruby this will translate to a hidden tag erb
+      #it will be added to params as song => {artist_id => value}
+      #the value will be whatever is song.artist_id
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
 end
---
+++ app/views/layouts/songs_helper.rb
@@ -0,0 +1,12 @@
+module SongsHelper
+
+end
+
+# when nested under artists
+#   /artists/:artist_id/songs/:id/edit
+#     doesn't display an artist select (FAILED - 1)
+
+# /songs/:id/edit
+# has a selector for artist (FAILED - 2)
+
+#artistshelper should respond to artist_select with 2 arguments (FAILED - 3)
